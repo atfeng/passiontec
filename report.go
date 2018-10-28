4de978c76f4a6f2ae215bca1837034a642efa9bf
ATFeng <i@atfeng.com>
2018-10-29 00:31:33 +0800

    添加支付方式的注释

--- report.go
+++ report.go
@@ -9,38 +9,38 @@ import (
 )
 
 type PayReport struct {
-	CashMoney              int
+	CashMoney              int //现金
 	CashProportion         string
 	CashOpValue            int
-	WechatMoney            int
+	WechatMoney            int //微信
 	WechatProportion       string
 	WechatOpValue          int
-	AlipayMoney            int
+	AlipayMoney            int //支付宝
 	AlipayProportion       string
 	AlipayOpValue          int
-	VipMainMoney           int
+	VipMainMoney           int //会员消费
 	VipMainProportion      string
 	VipMainOpValue         int
-	OncreditMoney          int
+	OncreditMoney          int //信用卡
 	OncreditProportion     string
 	OncreditOpValue        int
-	BankCardMoney          int
+	BankCardMoney          int //银行卡
 	BankCardMoneyDetail    string
 	BankCardProportion     string
 	BankCardOpValue        int
-	ThirdpayMoney          int
+	ThirdpayMoney          int //第三方支付
 	ThirdpayMoneyDetail    string
 	ThirdpayProportion     string
 	ThirdpayOpValue        int
-	CouponpayMoney         int
+	CouponpayMoney         int //优惠券
 	CouponpayMoneyDetail   string
 	CouponpayProportion    string
 	CouponpayOpValue       int
-	GroupbuypayMoney       int
+	GroupbuypayMoney       int //团购
 	GroupbuypayMoneyDetail string
 	GroupbuypayProportion  string
 	GroupbuypayOpValue     int
-	MoneyTotal             int
+	MoneyTotal             int //总计
 }
 
 func (cli *Client) GetPayReportDetail(start, end time.Time) (*PayReport, error) {
